6ab5973ccc8938167f5f3ee565fc828c264c4132
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-06-14 20:48:20 -0300

    organização de arquivos

---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Timeline from "./pages/Timeline";
+
+
+function App() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/timeline' element={<Timeline />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
