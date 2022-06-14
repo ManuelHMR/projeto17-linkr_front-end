70c363816108cba614ffe12c5b241c9bb8d46c27
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-06-14 20:26:42 -0300

    component created

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Timeline from "./pages/Timeline";
 
+import TrendingTags from "./tagsBox";
+
 
 function App() {
 
---
+++ src/components/tagsBox.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+function TrendingTags(props) {
+  return (
+    <>
+      <Box>
+       
+      </Box>
+    </>
+  );
+}
+
+export default TrendingTags;
+
+const Box = styled.div`
+  width: 301px;
+  height: 406px;
+  display: flex;
+  flex-direction: column;
+  background-color: #171717;
+  border-radius: 16px;
+  padding: 3px 3px;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
