e5ac0896d25a49111d698bbeecec6c4f434f5691
João Sarmento <joaovicentesarm@gmail.com>
2022-06-15 08:57:21 -0300

    Correção para deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './reset.css';
-import './index.css';
+import './assets/css/reset.css';
+import './assets/css/index.css';
 import App from './components/App';
 
 
