643fb6dcc6ce672829043646d6f2aff5a3f36123
ManuelHMR <manuelhmrjf@gmail.com>
2022-06-15 15:51:44 -0300

    fix: files location

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Timeline from "./pages/Timeline";
+import Signup from "../pages/Signup";
+import Timeline from "../pages/Timeline";
 
 
 function App() {
