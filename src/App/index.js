233883566f392e94bc143d8cfff31dab5534d458
João Sarmento <joaovicentesarm@gmail.com>
2022-06-15 09:04:46 -0300

    Atualização da branch

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Timeline from "./pages/Timeline";
+import Timeline from "../pages/Timeline";
 
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/reset.css';
 import './assets/css/index.css';
-import App from './components/App';
+import App from './App';
 
 
 const root = document.querySelector(".root");
