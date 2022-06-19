ce83eac6d716f57d7a66f3d12b519d0de1b79ca1
Ru Yi Shen <ruyishen2012@gmail.com>
2022-06-19 10:45:00 -0300

    feat : like post function - status ok

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,103 +1,173 @@
 import styled from "styled-components";
 import urlMetadata from "url-metadata";
+import ReactTooltip from "react-tooltip";
+import ReactModal from "react-modal";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Post(infos) {
-    const { username, url, pictureURL, text } = infos;
-    metadata(url);
-    return (
-        <PostContainer>
-            <img src={pictureURL} alt="Foto de perfil"></img>
-            <PostInfos>
-                <h4>{username}</h4>
-                <p>{text}</p>
-                <Link>
-                    <div>
-                        <h5>Como aplicar o Material UI em um projeto React</h5>
-                        <p>Hey! I have moved this tutorial to my personal blog. Same content, new location. Sorry about making you click through to another page.</p>
-                        <p>{url}</p>
-                    </div>
-                    <img src={pictureURL} alt="Foto de perfil"></img>
-                </Link>
-            </PostInfos>
-        </PostContainer>
-    )
+  const { username, url, pictureURL, text } = infos;
+  let id = 1;
+  const [liked, setLiked] = useState(false);
+  const URL = "https://projeto17-linkr-back-end.herokuapp.com/";
+  const token = localStorage.getItem("token");
+  metadata(url);
+
+
+  function likePost() {
+    
+    let newURL = URL;
+    if (!liked) {
+      newURL = URL + "like/" + id;
+    } else {
+      newURL = URL + "dislike/" + id;
+    }
+    setLiked(!liked);
+    console.log(newURL);
+    axios
+      .post(newURL, {}, { headers: { Authorization: `Bearer ${token}` } })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((e) => console.log(e));
+  }
+
+  return (
+    <PostContainer>
+      <img src={pictureURL} alt="Foto de perfil"></img>
+      <PostInfos>
+        <h4>{username}</h4>
+        <p>{text}</p>
+        <Link>
+          <div>
+            <h5>Como aplicar o Material UI em um projeto React</h5>
+            <p>
+              Hey! I have moved this tutorial to my personal blog. Same content,
+              new location. Sorry about making you click through to another
+              page.
+            </p>
+            <p>{url}</p>
+          </div>
+          <img src={pictureURL} alt="Foto de perfil"></img>
+        </Link>
+      </PostInfos>
+      <ion-icon name="trash"></ion-icon>
+      <Heart onClick={likePost} liked={liked} data-tip="1000 curtidas">
+        {liked ? (
+          <ion-icon name="heart"></ion-icon>
+        ) : (
+          <ion-icon name="heart-outline"></ion-icon>
+        )}
+        <p>1000 likes</p>
+      </Heart>
+      <ReactTooltip place="bottom" type="light" effect="solid" />
+    </PostContainer>
+  );
 }
 
 function metadata(url) {
-    urlMetadata("http://bit.ly/2ePIrDy").then((result) => console.log(result));
+  urlMetadata("http://bit.ly/2ePIrDy").then((result) => console.log(result));
 }
 
-
-
 const PostContainer = styled.div`
-    width: 611px;
-    height: auto;
-    padding: 17px 18px;
-    background: #171717;
-    display: flex;
-    border-radius: 16px;
-    margin: 8px 0;
-    color: #FFFFFF;
-    font-family: 'Lato', sans-serif;
-    font-weight: 300;
+  position: relative;
+  width: 611px;
+  height: auto;
+  padding: 17px 18px;
+  background: #171717;
+  display: flex;
+  border-radius: 16px;
+  margin: 8px 0;
+  color: #ffffff;
+  font-family: "Lato", sans-serif;
+  font-weight: 300;
 
-    img{
-        height: 50px;
-        width: 50px;
-        border-radius: 25px;
-        margin-right: 18px;
-    }
-`
+  img {
+    height: 50px;
+    width: 50px;
+    border-radius: 25px;
+    margin-right: 18px;
+  }
+  & > ion-icon {
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    size: 20px;
+  }
+`;
 
 const PostInfos = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-    h4{
-        margin-bottom: 7px;
-        font-weight: 400;
-    }
+  h4 {
+    margin-bottom: 7px;
+    font-weight: 400;
+  }
 
-    p{
-        color: #B7B7B7;
-        padding-right: 30px;
-    }
-`
+  p {
+    color: #b7b7b7;
+    padding-right: 30px;
+  }
+`;
 
 const Link = styled.div`
+  height: 155px;
+  width: 503px;
+  border: 1px solid #4d4d4d;
+  border-radius: 11px;
+  margin: 20px 0 10px 0;
+  font-family: "Lato", sans-serif;
+  font-weight: 400;
+  position: relative;
+
+  div {
+    width: 300px;
+    display: flex;
+    flex-direction: column;
+    margin-left: 20px;
+  }
+  p {
+    font-size: 11px;
+    margin-bottom: 13px;
+  }
+  h5 {
+    font-size: 16px;
+    margin-top: 24px;
+    margin-bottom: 5px;
+  }
+  img {
+    border-radius: 0px 9px 13px 0px;
+    width: 153.44px;
     height: 155px;
-    width: 503px;
-    border: 1px solid #4D4D4D;
-    border-radius: 11px;
-    margin: 20px 0 10px 0;
-    font-family: 'Lato', sans-serif;
-    font-weight: 400;
-    position: relative;
+    position: absolute;
+    right: 0;
+    top: 0;
+    margin: 0;
+  }
+`;
 
-    div{
-        width: 300px;
-        display: flex;
-        flex-direction: column;
-        margin-left: 20px;
-    }
-    p{
-        font-size: 11px;
-        margin-bottom: 13px;
+const Heart = styled.div`
+  width: 50px;
+  height: 50px;
+  margin-left: 18px;
+  position: absolute;
+  left: 0;
+  top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  p{
+    margin-top: 5px;
+    font-size: 11px;
+  }
 
-    }
-    h5{
-
-        font-size:  16px;
-        margin-top: 24px;
-        margin-bottom: 5px;
-    }
-    img{
-        border-radius: 0px 9px 13px 0px;
-        width: 153.44px;
-        height: 155px;
-        position: absolute;
-        right: 0;
-        top: 0;
-        margin: 0;
-    }
-`
+  ion-icon {
+    font-size: 30px;
+    color: ${(props) => (props.liked ? "red" : "#FFFFFF")};
+  }
+  :hover {
+    cursor: pointer;
+  }
+`;
