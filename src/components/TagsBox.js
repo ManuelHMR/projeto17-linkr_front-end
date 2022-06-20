519bbb8a1273f300429492a7415bf78c64332d96
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-06-20 19:55:35 -0300

    fix: import

--- src/components/TagsBox.js
+++ src/components/TagsBox.js
@@ -1,4 +1,4 @@
-import ReactHashtag from "react-hashtag";
+import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
